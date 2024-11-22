643d0bc99fb0f07e8a68afeb640bd5ca8bbb5e37
FernandoLabastida <labastidafernando460@gmail.com>
2024-11-21 21:59:08 -0600

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Home(){
+  return (
+    <Box>
+      <Typography variant="h1">
+        Hola
+      </Typography>
+    </Box>
+  )
+}

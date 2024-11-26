33290b3af0cb5eac70740d33f822223d224bc747
Segura Garduño Karen Alin <54106056+KarenAlinSegura@users.noreply.github.com>
2024-11-26 14:59:12 -0600

    Create contac.jsx

---
+++ app/styles/contac.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Box, Typography } from "@mui/material";
+
+export default function contact() {
+  return (
+    <Box>
+      <Typography variant="h1">Hola</Typography>
+      <Typography variant="h1">Hola</Typography>
+      <Typography variant="h1">Hola</Typography>
+      <Typography variant="h1">Hola</Typography>
+      <Typography variant="h1">Hola</Typography>
+    </Box>
+  );
+}
